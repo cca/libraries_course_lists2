af25f6ff8900ccc5faf7b870534a27c25cd64066
phette23 <phette23@gmail.com>
2019-09-09 17:52:39 -0700

    script to write faculty LDAP group text files

---
+++ faculty_groups.py
@@ -0,0 +1,30 @@
+""" given Workday Student JSON data, update EQUELLA groups to reflect who is
+teaching in what programs. Right now the script only writes out a series of .txt
+files named after LDAP groups; these are lists of users to be sent to the Help
+Desk so they can update all the LDAP groups. I want to add functionality such
+that it also updates the VAULT faculty groups for each department.
+
+usage: python faculty_groups.py data/data.json
+"""
+import json
+import sys
+
+from lib import Course, map
+
+with open(sys.argv[1], 'r') as file:
+    data = json.load(file)
+    courses = [Course(**d) for d in data]
+
+# mapping of dept code to list of faculty usernames e.g. "LIBRA": ["ephetteplace"]
+teaching = {}
+for course in courses:
+    # initialize department list if it doesn't exist yet
+    if teaching.get(course.owner) == None:
+        teaching[course.owner] = set([i["username"] for i in course.instructors])
+    else:
+        teaching[course.owner].update([i["username"] for i in course.instructors])
+
+# write LDAP text files
+for dept, instructors in teaching.items():
+    with open('data/{}.txt'.format(map[dept]["ldap"]), 'w') as file:
+        file.write('\n'.join(list(instructors)))
