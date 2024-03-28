7b6b61c5e4711374597f277cf681f4069bf247a5
phette23 <phette23@gmail.com>
2024-03-28 14:03:37 -0700

    fix: close requests session in test_utilities
    also python formatting for utilities

--- lib/utilities.py
+++ lib/utilities.py
@@ -1,34 +1,35 @@
 import re
 
-import requests
+from requests import Session
 
 import config
 
 
-PORTAL_STATUSES = ('Closed', 'Open', 'Waitlist')
+PORTAL_STATUSES = ("Closed", "Open", "Waitlist")
 
-def request_wrapper():
+
+def request_wrapper() -> Session:
     if not config.token:
-        raise Exception('I need an OAuth token in config.py to work.')
+        raise Exception("I need an OAuth token in config.py to work.")
 
-    s = requests.Session()
+    s = Session()
     headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-Authorization': 'access_token=' + config.token,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-Authorization": "access_token=" + config.token,
     }
     s.headers.update(headers)
     return s
 
 
-def strip_prefix(string):
+def strip_prefix(string) -> str:
     """
     A lot of Workday names, which we are unfortunately forced to use as identifiers,
     begin with an "AX_..." prefix as in "AU_CERAM" (for the Ceramics academic unit)
     or "AP_Fall_2019" for the Fall academic period. We need these names for
     matching but don't use this prefix.
     """
-    return re.sub(r'^A[A-Z]_', '', string)
+    return re.sub(r"^A[A-Z]_", "", string)
 
 
 def course_sort(course):
--- test/test_utilities.py
+++ test/test_utilities.py
@@ -9,27 +9,40 @@ class TestStripPrefix(unittest.TestCase):
     # strip_prefix is used in two contexts right now: for department codes but
     # also for academic periods (semesters)
     def test_department(self):
-        self.assertEqual(strip_prefix('AU_CERAM'), 'CERAM')
-
+        self.assertEqual(strip_prefix("AU_CERAM"), "CERAM")
 
     def test_semester(self):
-        self.assertEqual(strip_prefix('AP_Spring_2020'), 'Spring_2020')
+        self.assertEqual(strip_prefix("AP_Spring_2020"), "Spring_2020")
 
 
 class TestCourseSort(unittest.TestCase):
     courses = []
 
     def setUp(self):
-        with open('test/courses-fixture.json', 'r') as file:
+        with open("test/courses-fixture.json", "r") as file:
             data = json.load(file)
             self.courses = [Course(**c) for c in data]
 
     # strip_prefix is used in two contexts right now: for department codes but
     # also for academic periods (semesters)
     def test_course_sort(self):
-        c = self.courses # just to save typing below
+        c = self.courses  # just to save typing below
         sorted_courses = sorted(c, key=course_sort)
-        correct_sort = [c[1], c[12], c[9], c[11], c[3], c[2], c[10], c[0], c[7], c[8], c[5], c[6], c[4],]
+        correct_sort = [
+            c[1],
+            c[12],
+            c[9],
+            c[11],
+            c[3],
+            c[2],
+            c[10],
+            c[0],
+            c[7],
+            c[8],
+            c[5],
+            c[6],
+            c[4],
+        ]
         self.assertEqual(correct_sort, sorted_courses)
 
 
@@ -39,16 +52,13 @@ class TestRequestWrapper(unittest.TestCase):
         # make "unclosed socket" warnings stop, see this for instance
         # https://github.com/boto/boto3/issues/454
         warnings.filterwarnings(
-            "ignore",
-            category=ResourceWarning,
-            message="unclosed.*<socket.socket.*>"
+            "ignore", category=ResourceWarning, message="unclosed.*<socket.socket.*>"
         )
-
 
     def test_request_wrapper(self):
         global config
         s = request_wrapper()
-        r = s.get(config.api_root + '/taxonomy')
+        r = s.get(config.api_root + "/taxonomy")
         r.raise_for_status()
         taxos = r.json()
         self.assertTrue(taxos["length"] > 1)
@@ -57,7 +67,8 @@ class TestRequestWrapper(unittest.TestCase):
         with self.assertRaises(Exception):
             del config.token
             s = request_wrapper()
+        s.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
