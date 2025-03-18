36f9f9bc26e0813b6bf4792c6fd61fce85e9bdb6
phette23 <phette23@gmail.com>
2025-03-18 10:04:09 -0700

    style: type hints for Course class

--- lib/course.py
+++ lib/course.py
@@ -9,8 +9,8 @@ from .utilities import strip_prefix, PORTAL_STATUSES
 
 class Course(SimpleNamespace):
 
-    def __repr__(self):
-        return "{} {} {}".format(self.semester, self.section_code, self.section_title)
+    def __repr__(self) -> str:
+        return f"{self.semester} {self.section_code} {self.section_title}"
 
     # some Workday fields have encoded entities (e.g. "&amp;") so we want to
     # unescape _all_ string attributes before returning them
@@ -20,7 +20,8 @@ class Course(SimpleNamespace):
             return unescape(value)
         return value
 
-    def find_colocated_sections(self, courses):
+    # quote "Course" because it is not defined yet, causes a NameError
+    def find_colocated_sections(self, courses: list["Course"]) -> list["Course"]:
         """Return colocated/cross-listed sections
 
         Parameters
@@ -45,8 +46,8 @@ class Course(SimpleNamespace):
         return sections
 
     @property
-    def instructor_names(self):
-        # print list of instructors as a comma-separated string of names
+    def instructor_names(self) -> str:
+        """list of instructors as a comma-separated string of names"""
         if len(self.instructors) == 0:
             return "[instructors to be determined]"
         names = []
@@ -55,12 +56,12 @@ class Course(SimpleNamespace):
         return ", ".join(names)
 
     @property
-    def instructor_usernames(self):
-        # print list of instructor usernames as comma-separated string
+    def instructor_usernames(self) -> str:
+        """list of instructor usernames as comma-separated string"""
         return ", ".join([i["username"] for i in self.instructors])
 
     @property
-    def on_portal(self):
+    def on_portal(self) -> bool:
         """boolean for whether a course is included in Portal course catalog"""
         if (
             self.hidden != "1"
@@ -71,7 +72,8 @@ class Course(SimpleNamespace):
         return False
 
     @property
-    def owner(self):
+    def owner(self) -> str:
+        """Five-letter department code for course's primary department"""
         for au in self.academic_units:
             if au["course_owner"]:
                 # 2 exceptions: HAAVC use "AU_VISST" & ETHST/ETHSM use "AU_DIVST"
@@ -92,5 +94,6 @@ class Course(SimpleNamespace):
         return False
 
     @property
-    def semester(self):
+    def semester(self) -> str:
+        """Human-readable term like "Fall 22025"."""
         return strip_prefix(self.term).replace("_", " ")
